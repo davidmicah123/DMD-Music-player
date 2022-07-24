c28d4ef40d402c82d1b948cd3853baa506cbeb42
David Micah-Daniels <98872908+davidmicah123@users.noreply.github.com>
2022-07-24 04:36:51 +0100

    Adding page functionality

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+const audio1 = document.querySelector(".audio_1");
+const audio1Text = document.querySelector(".audio1_text");
+const audio2Text = document.querySelector(".audio2_text");
+const audio3Text = document.querySelector(".audio3_text");
+const audio4Text = document.querySelector(".audio4_text");
+const audio5Text = document.querySelector(".audio5_text");
+const volume = document.querySelector(".volume");
+const pauseAudio = document.querySelector(".pause_audio");
+const playAudio = document.querySelector(".play_audio");
+const currentMusic = document.querySelector(".current_music");
